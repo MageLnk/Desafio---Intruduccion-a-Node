f2ea2e32cd9c85618b2f975d87212792d28055fa
MageLink <mage.link.zhezard@gmail.com>
2022-12-14 23:37:18 -0300

    Delete carpeta

--- Práctica/operaciones.js
+++
@@ -1,8 +0,0 @@
-const fs = require("fs");
-const createFile = (nameFile, contentFile) => {
-  fs.writeFileSync(`${nameFile}.json`, JSON.stringify(contentFile));
-};
-
-const readFile = (nameFile) => JSON.parse(fs.readFileSync(`${nameFile}.json`, "utf8"));
-
-module.exports = { createFile, readFile };
