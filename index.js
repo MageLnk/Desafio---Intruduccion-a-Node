37eee6bca896996259d9bb1c4f25ee45285e8bad
MageLink <mage.link.zhezard@gmail.com>
2022-12-16 20:47:40 -0300

    Little package color added

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
-const { createFile, readFile } = require("./operaciones");
+const { createFile, readFile, validatingYesOrNo } = require("./operaciones");
+const { preguntas } = require("./preguntas");
+require("colors");
 
 //createFile("Wanda", "5 años", "Gata", "Blanca", "Vomito blanco");
 
-console.log(readFile());
+//console.log(readFile());
+
+console.clear();
+console.log("Bienvenido a la veterinaria GuauGuau.".red);
+
+const app = () => {
+  console.log("Ola k ase");
+};
+
+app();
+
+/*
+process.stdin.on("data", (data) => {
+  console.log("data", data.toString().trim());
+  process.exit();
+});
+
+*/
